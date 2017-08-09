3dab1501b9f46fe80cfdd57d39502ae2de804433
Rob Denroche <rdenroche@oicr.on.ca>
2017-08-09 16:26:33 -0400

    changing paths to point at production dir

--- runPlots.sh
+++ runPlots.sh
@@ -3,16 +3,18 @@ sum=$1
 vars=$2
 outpath=$3
 
-/.mounts/labs/PCSI/users/rdenroche/phoenixReporter/plotIndelSVbars.pl $sum $outpath
-#/.mounts/labs/PCSI/users/rdenroche/phoenixReporter/plotChromothripsis.pl
-/.mounts/labs/PCSI/users/rdenroche/phoenixReporter/plotHistograms.pl $sum $outpath
-/.mounts/labs/PCSI/users/rdenroche/phoenixReporter/plotHallmarkBars.pl $sum $outpath
-/.mounts/labs/PCSI/users/rdenroche/phoenixReporter/plotCosmicSignnlsBars.pl $sum $outpath
-/.mounts/labs/PCSI/users/rdenroche/phoenixReporter/plotCelluloid.pl $sum $outpath
-/.mounts/labs/PCSI/users/rdenroche/phoenixReporter/plotOncoSlice.pl $sum $vars $outpath
-/.mounts/labs/PCSI/users/rdenroche/phoenixReporter/plotSNVbars.pl $sum $outpath
-/.mounts/labs/PCSI/users/rdenroche/phoenixReporter/plotSSMfreqs.pl $sum $outpath
+prodpath=/.mounts/labs/PCSI/production/phoenix-report
+
+${prodpath}/plotIndelSVbars.pl $sum $outpath
+#${prodpath}/plotChromothripsis.pl
+${prodpath}/plotHistograms.pl $sum $outpath
+${prodpath}/plotHallmarkBars.pl $sum $outpath
+${prodpath}/plotCosmicSignnlsBars.pl $sum $outpath
+${prodpath}/plotCelluloid.pl $sum $outpath
+${prodpath}/plotOncoSlice.pl $sum $vars $outpath
+${prodpath}/plotSNVbars.pl $sum $outpath
+${prodpath}/plotSSMfreqs.pl $sum $outpath
 
 # last 'cause slow
-/.mounts/labs/PCSI/users/rdenroche/phoenixReporter/plotWholeGenome.pl $sum $outpath
+${prodpath}/plotWholeGenome.pl $sum $outpath
 
